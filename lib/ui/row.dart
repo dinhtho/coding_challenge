ad53b889cf3b86a41302dcd93350a2d81ad8e3c3
dinhtho <tho.nguyen@beesightsoft.com>
2019-04-27 21:07:42 +0700

    check position before rebuiding item

--- lib/ui/row.dart
+++ lib/ui/row.dart
@@ -3,35 +3,56 @@ import 'package:coding_challenge/model/data.dart';
 import 'package:coding_challenge/bloc/row_bloc.dart';
 import 'package:coding_challenge/bloc/app/app_bloc_provider.dart';
 
-class MyRow extends StatelessWidget {
+class MyRow extends StatefulWidget {
   Data data;
 
   MyRow(@required this.data);
 
   @override
-  Widget build(BuildContext context) {
-    final rowBloc = AppBlocProvider.of(context).rowBloc;
+  State<StatefulWidget> createState() => MyRowState();
+}
+
+class MyRowState extends State<MyRow> {
+  Stream stream;
+  Data data;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback(onLayoutDone);
+  }
+
+  void onLayoutDone(_) {
     final detailViewBloc = AppBlocProvider.of(context).detailViewBloc;
-    return StreamBuilder(
-        stream: detailViewBloc.updateRow,
-        builder: (context, snapshot) {
-          if (snapshot?.data != null &&
-              snapshot?.data?.position == data.position) {
-            data = snapshot.data;
-          }
-          return GestureDetector(
-            onTap: () => rowBloc.postRowData(data),
-            child: Container(
-              color: data.color,
-              width: 100,
-              height: double.infinity,
-              child: Center(
-                  child: Text(
-                data.value.toString(),
-                style: TextStyle(fontSize: 30),
-              )),
-            ),
-          );
+    detailViewBloc.updateRow.listen((e) {
+      if (e != null && e is Data && e.position == data.position) {
+        setState(() {
+          data = e;
         });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    data = widget.data;
+    final rowBloc = AppBlocProvider.of(context).rowBloc;
+
+    return GestureDetector(
+      onTap: () {
+        rowBloc.postRowData(data);
+      },
+      child: Container(
+        color: data.color,
+        width: 100,
+        height: double.infinity,
+        child: Center(
+            child: Text(
+          data.value.toString(),
+          style: TextStyle(fontSize: 30),
+        )),
+      ),
+    );
   }
 }
