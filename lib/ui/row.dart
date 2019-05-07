8a7a8c58b95a5e0d0ce467d2a8afbc6a9c22c788
dinhtho <tho.nguyen@beesightsoft.com>
2019-05-07 16:55:59 +0700

    add filter to stream

--- lib/ui/row.dart
+++ lib/ui/row.dart
@@ -14,7 +14,6 @@ class MyRow extends StatefulWidget {
 
 class MyRowState extends State<MyRow> {
   Data data;
-  HomePageBloc homePageBloc;
 
   @override
   void initState() {
@@ -23,33 +22,25 @@ class MyRowState extends State<MyRow> {
   }
 
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-
-    homePageBloc = AppBlocProvider.of(context).homePageBloc;
-    homePageBloc.updateRow.listen((e) {
-      if (e != null && e is Data && e.position == data.position) {
-        setState(() {
-          data = e;
-        });
-      }
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => homePageBloc.postRowData(data),
-      child: Container(
-        color: data.color,
-        width: 100,
-        height: double.infinity,
-        child: Center(
-            child: Text(
-          data.value.toString(),
-          style: TextStyle(fontSize: 30),
-        )),
-      ),
-    );
+    var homePageBloc = AppBlocProvider.of(context).homePageBloc;
+
+    return StreamBuilder(stream: homePageBloc.updateRow.skipWhile((e) {
+      return e.position != data.position;
+    }), builder: (context, snapshot) {
+      return GestureDetector(
+        onTap: () => homePageBloc.postRowData(data),
+        child: Container(
+          color: data.color,
+          width: 100,
+          height: double.infinity,
+          child: Center(
+              child: Text(
+            data.value.toString(),
+            style: TextStyle(fontSize: 30),
+          )),
+        ),
+      );
+    });
   }
 }
